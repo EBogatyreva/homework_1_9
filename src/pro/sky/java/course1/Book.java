0ae90efaf95b1caf211437b7281172b9bc6ab0ba
L <EBogatyrevaPK@yandex.ru>
2022-02-24 14:43:12 +1200

    Я чуть поправила, переменные конструктора занова переименовала как и переменные класса, тк в ином случае теряется смысл this)))

--- src/pro/sky/java/course1/Book.java
+++ src/pro/sky/java/course1/Book.java
@@ -7,24 +7,24 @@ public class Book {
     private int yearOfPublication;
 
     //___конструкторы для класса Book
-    public Book(String n, int year, String f, String s) {
+    public Book(String n, int year, String f, String s) {//если переменные названны по другому надобюнгость в this отпадает
         this.name = n;
         this.yearOfPublication = year;
         author.setName(f, s);
     }
 
-    public Book(String n, String f, String s) {
-        this.name = n;
-        author.setName(f, s);
+    public Book(String name, String firstName, String surName) {
+        this.name = name;
+        author.setName(firstName, surName);
     }
 
-    public Book(String n, int year) {
-        this.name = n;
-        this.yearOfPublication = year;
+    public Book(String name, int yearOfPublication) {
+        this.name = name;
+        this.yearOfPublication = yearOfPublication;
     }
 
-    public Book(String n) {
-        this.name = n;
+    public Book(String name) {
+        this.name = name;
     }
 //___________________________
 
@@ -36,7 +36,7 @@ public class Book {
         return yearOfPublication;
     }
 
-    public void setYearOfPublication(int a) {
-        this.yearOfPublication = a;
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
     }
 }
